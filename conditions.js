b5f7a75e9e208ce9d70c8f5f8d6980375da03bfd
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2026-01-11 23:04:11 +0300

    added conditions

---
+++ conditions.js
@@ -0,0 +1,35 @@
+// ============if-else============
+
+// if (true) {
+// 	console.log('true');
+// } else if (false) {
+// 	console.log('false');
+// } else {
+// 	console.log('else');
+// }
+
+// ============switch-case============
+
+// switch (true) {
+// 	case true:
+// 		console.log('true');
+// 		break;
+// 	case false:
+// 		console.log('false');
+// 		break;
+// 	default:
+// 		console.log('default');
+// 		break;
+// }
+
+// ============abstract-equality-condition============
+
+// console.log(1 == '1'); // true, implicit type conversion
+
+// ============strict-equality-condition============
+
+// console.log(1 === '1'); // false, strict type check
+
+// ============ternary-operator============
+
+// console.log(true ? 'true' : 'false');
