9b845a63ba3a73626ded6656e7d38e637e9d52ba
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2026-01-11 23:20:41 +0300

    added type-conversion

---
+++ type-conversion.js
@@ -0,0 +1,12 @@
+// =================type-conversion=================
+// console.log(typeof ('1' + 1)); // 11 string
+// console.log(typeof ('1' - 1)); // 0 number
+// console.log(typeof ('1' * 1)); // 1 number
+// console.log(typeof ('1' / 1)); // 1 number
+// console.log(typeof +'1'); // 1 number
+// console.log(typeof Number('1')); // 1 number
+// console.log(typeof JSON.stringify('1')); // 1 number
+// console.log(typeof '1'.toString()); // 1 number
+// console.log(typeof parseInt('1.1')); // 1 number
+// console.log(typeof parseFloat('1.1')); // 1.1 number
+// console.log(typeof !!'1'); // true boolean
