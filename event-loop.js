1597ae0343171ab20f390f8ad2d03a2ab74070e4
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2026-01-18 16:16:37 +0300

    added event loop

---
+++ event-loop.js
@@ -0,0 +1,39 @@
+// ===========event-loop===========
+
+// console.log(1);
+
+// setTimeout(() => {
+// 	Promise.resolve(console.log(2));
+// }, 1000);
+
+// setTimeout(() => {
+// 	console.log(3);
+// }, 0);
+
+// Promise.resolve().then(() => {
+// 	console.log(4);
+// });
+
+// setTimeout(() => {
+// 	Promise.resolve().then(() => console.log(5));
+// }, 0);
+
+// (async function () {
+// 	const res = await Promise.resolve(6);
+// 	console.log(res);
+// })();
+
+// const res = Promise.resolve(7);
+// console.log(res);
+
+// setTimeout(() => {
+// 	console.log(8);
+// }, 1000);
+
+// res.then((data) => console.log(data));
+
+// setTimeout(() => {
+// 	console.log(9);
+// }, 0);
+
+// console.log(10);
